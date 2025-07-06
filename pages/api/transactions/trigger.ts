37f15b49365e442ca93147b5362223195b4f73c4
c4dr-me <142721314+c4dr-me@users.noreply.github.com>
2025-07-06 16:39:08 +0530

    config pusher

--- pages/api/transactions/trigger.ts
+++ pages/api/transactions/trigger.ts
@@ -2,10 +2,10 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import Pusher from 'pusher';
 
 const pusher = new Pusher({
-  appId: process.env.NEXT_PUBLIC_PUSHER_APP_ID!,
-  key: process.env.NEXT_PUBLIC_PUSHER_KEY!,
-  secret: process.env.NEXT_PUBLIC_PUSHER_SECRET!,
-  cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER!,
+  appId: process.env.PUSHER_APP_ID!,
+  key: process.env.PUSHER_KEY!,
+  secret: process.env.PUSHER_SECRET!,
+  cluster: process.env.PUSHER_CLUSTER!,
   useTLS: true,
 });
 
