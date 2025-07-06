8728bf095e37f77214227dc8acd5548657634145
c4dr-me <142721314+c4dr-me@users.noreply.github.com>
2025-07-06 16:49:30 +0530

    added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Toaster } from "@/components/ui/sonner";
 import { BudgetProvider } from "@/hooks/useBudgetContext";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -43,6 +44,7 @@ export default function RootLayout({
             },
           }}
         />
+        <Analytics />
       </body>
     </html>
   );
